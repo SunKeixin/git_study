cc32fa15fe6b655d23fa467f578a4e80639f896b
孙启新[sunqixin] <sunqixin@xiaomi.com>
2020-08-07 19:25:33 +0800

    提交

--- src/main/java/Git_Test1.java
+++ src/main/java/Git_Test1.java
@@ -3,5 +3,6 @@ public class Git_Test1 {
         System.out.println("我是test1第一次提交");
         System.out.println("我是master，做一次修改提交");
         System.out.println("我在test1修改了。。。");
+        System.out.println("测试git add -i test1");
     }
 }
--- src/main/java/Git_Test2.java
+++ src/main/java/Git_Test2.java
@@ -1,5 +1,5 @@
 public class Git_Test2 {
     public static void main(String[] args) {
-
+        System.out.println("测试git add -i test2");
     }
 }
